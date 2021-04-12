2a40882e26356a8579a9d2286c2e68df95e56d25
remyfabre <47010716+remyfabre@users.noreply.github.com>
2021-04-12 17:28:59 +0200

    Update landing.js

--- landing.js
+++ landing.js
@@ -1,13 +1,23 @@
 object = document.getElementById("goingnext")
+object_2 = document.getElementById("goingnext-2")
 
 object.type = "submit"
+object_2.type = "submit"
 
 $("#goingnext").addClass("html-embed-2 homepage button-3-offer button-block-3 bg-primary-3-offer w-button")
+$("#goingnext_2").addClass("html-embed-2 homepage button-3-offer button-block-3 bg-primary-3-offer w-button")
 
 object.addEventListener("click", ErrorMessage);
+object_2.addEventListener("click", ErrorMessage);
 
 function ErrorMessage() {
   if (!document.getElementById("autocomplete").value.match(/^\d/)) {
+    document.getElementById("card-body-2").style.display = 'block';
+    setTimeout(function(){
+      document.getElementById("card-body-2").style.display = 'none';
+    }, 5000);
+  }
+  if (!document.getElementById("autocomplete_2").value.match(/^\d/)) {
     document.getElementById("card-body-2").style.display = 'block';
     setTimeout(function(){
       document.getElementById("card-body-2").style.display = 'none';
@@ -97,6 +107,7 @@ function initAutocomplete() {
   // adresses and France.
   // Address must start with a number to start Autocomplete
   var input = document.getElementById('autocomplete');
+  var input_2 = document.getElementById('autocomplete_2');
 
   var options = {
     types: ['address'],
@@ -104,14 +115,17 @@ function initAutocomplete() {
   };
 
   autocomplete = new google.maps.places.Autocomplete(input, options);
+  autocomplete_2 = new google.maps.places.Autocomplete(input_2, options);
 
   // Avoid paying for data that you don't need by restricting the set of
   // place fields that are returned to just the address components.
   autocomplete.setFields(['address_component']);
+  autocomplete_2.setFields(['address_component']);
 
   // When the user selects an address from the drop-down, save the
   // address fields in local storage.
   autocomplete.addListener('place_changed', fillInAddress);
+  autocomplete_2.addListener('place_changed', fillInAddress_2);
 }
 
 function patternMatching() {
@@ -149,7 +163,32 @@ function fillInAddress() {
   }
 }
 
+function fillInAddress_2() {
+  // Get the place details from the autocomplete object.
+  //alert(document.getElementById('autocomplete').innerHTML);
+  if (document.getElementById("autocomplete_2").value.match(/^\d/)) {
+    var place = autocomplete_2.getPlace();
+    // Get each component of the address from the place details,
+    // and then fill-in the corresponding field on the cookie.
+    for (var i = 0; i < place.address_components.length; i++) {
+      var addressType = place.address_components[i].types[0];
+      if (componentForm[addressType]) {
+        var val = encodeURIComponent(place.address_components[i][componentForm[addressType]]);
+        // Store the home address in a cookie
+        var homecookie = addressType + "=" + val;
+        var path = "path=/"
+        alert(homecookie + ';' + path)
+        document.cookie = homecookie + ';' + path;
+      }
+    }
+    window.location.assign("https://zefir-4f7ae76c36850c72251-c11aa8e2cbacb.webflow.io/offre/demande");
+  } else {
+    ErrorMessage();
+  }
+}
+
 var pac_input = document.getElementById('autocomplete');
+var pac_input_2 = document.getElementById('autocomplete_2');
 
 (function pacSelectFirst(input){
   // store the original event binding function
@@ -165,7 +204,14 @@ var pac_input = document.getElementById('autocomplete');
         var suggestion_selected = $(".pac-item-selected").length > 0;
         document.getElementById('goingnext').onclick = function() {
           if (!suggestion_selected && document.getElementById("autocomplete").value.match(/^\d/)) {
-          	alert("I went there")
+            var simulated_downarrow = $.Event("keydown", {keyCode:40, which:40});
+            var event = $.Event("enter", {keyCode:13, which:13})
+            orig_listener.apply(input, [simulated_downarrow]);
+            orig_listener.apply(input, [event]);
+          }
+        }
+        document.getElementById('goingnext_2').onclick = function() {
+          if (!suggestion_selected && document.getElementById("autocomplete_2").value.match(/^\d/)) {
             var simulated_downarrow = $.Event("keydown", {keyCode:40, which:40});
             var event = $.Event("enter", {keyCode:13, which:13})
             orig_listener.apply(input, [simulated_downarrow]);
